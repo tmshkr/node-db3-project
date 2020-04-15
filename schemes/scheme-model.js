f7a5148fe9e33b84637e1a00dc60337755afa9aa
Tim Shaker <tim@tmshkr.com>
2020-04-15 13:42:35 -0700

    GET /api/schemes/:id/steps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -17,7 +17,14 @@ function findById(id) {
   return db("schemes").where({ id }).first();
 }
 
-function findSteps() {}
+function findSteps(id) {
+  return db("schemes as sc")
+    .join("steps as st", "st.scheme_id", "sc.id")
+    .select("sc.scheme_name", "st.step_number", "st.instructions")
+    .where({ "sc.id": id })
+    .orderBy("st.step_number");
+}
+
 function add() {}
 function update() {}
 function remove() {}
