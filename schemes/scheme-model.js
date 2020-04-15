2cc742c9c29c5ddc1ea2252bd3474a595dfe3ed0
Tim Shaker <tim@tmshkr.com>
2020-04-15 13:15:58 -0700

    GET /api/schemes/:id

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,8 +2,22 @@ const db = require("../data/db-config.js");
 
 module.exports = {
   find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
 };
 
 function find() {
   return db("schemes");
 }
+
+function findById(id) {
+  return db("schemes").where({ id }).first();
+}
+
+function findSteps() {}
+function add() {}
+function update() {}
+function remove() {}
